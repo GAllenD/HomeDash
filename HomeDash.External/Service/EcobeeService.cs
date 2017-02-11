68ae661a10682ddb4a66b4233c802d511b9b2ab8
Greg Dzezinski <gregdzezinski@gmail.com>
2017-02-11 15:27:21 -0500

    same

---
+++ HomeDash.External/Service/EcobeeService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeDash.External.Service
+{
+    public class EcobeeService
+    {
+        public async void RefreshTokenAsync(string apiKey, string refreshToken)
+        {
+            var url = "https://api.ecobee.com/token";
+
+            var values = new Dictionary<string, string>
+            {
+                { "grant_type", "refresh_token" },
+                { "code", refreshToken },
+                { "client_id", apiKey }
+            };
+            var content = new FormUrlEncodedContent(values);
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var serviceResponse = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+    }
+}
